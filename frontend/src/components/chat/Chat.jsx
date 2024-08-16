4dbedb0bb78e38cd66817d433663e81a02ffde48
TNTeshka <pupikpn@gmail.com>
2024-08-16 14:50:45 +0300

    PrivateChatDialog design made

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,200 +1,228 @@
-import './styles.css';
+import "./styles.css";
 import React, { useEffect, useRef, useState } from "react";
 import { getAllMessages } from "../../utils/http";
 import Message from "./Message";
 import Cookies from "js-cookie";
 
 export default function Chat({ client, isConnected, setNotifications }) {
-    const [messages, setMessages] = useState([]);
-    const [error, setError] = useState(null);
-    const messageInputRef = useRef();
-    const chatContainerRef = useRef();
+  const [messages, setMessages] = useState([]);
+  const [error, setError] = useState(null);
+  const messageInputRef = useRef();
+  const chatContainerRef = useRef();
 
-    function onChatMessageReceived(message) {
-        const parsedMessage = JSON.parse(message.body);
-        if (parsedMessage.type) {
-            switch (parsedMessage.type) {
-                case 'CLEAR':
-                    if (parsedMessage.content.split(' ')[0] === 'All') {
-                        setMessages([]);
-                    } else {
-                        const clearCount = parseInt(parsedMessage.content.split(' ')[0], 10);
-                        setMessages((prevMessages) => prevMessages.slice(0, -clearCount));
-                    }
-                    return;
-                case 'DELETE':
-                    return;
-                default:
-                    return;
-            }
-        }
-        const { id, sender, content, timestamp, receiver } = parsedMessage;
-        setMessages((prevMessages) => {
-            const newMessages = [...prevMessages, {
-                id: id,
-                sender: sender,
-                content: content,
-                timestamp: timestamp,
-                receiver: receiver
-            }];
-            return newMessages.length > 80 ? newMessages.slice(-80) : newMessages;
-        });
+  function onChatMessageReceived(message) {
+    const parsedMessage = JSON.parse(message.body);
+    if (parsedMessage.type) {
+      switch (parsedMessage.type) {
+        case "CLEAR":
+          if (parsedMessage.content.split(" ")[0] === "All") {
+            setMessages([]);
+          } else {
+            const clearCount = parseInt(
+              parsedMessage.content.split(" ")[0],
+              10
+            );
+            setMessages((prevMessages) => prevMessages.slice(0, -clearCount));
+          }
+          return;
+        case "DELETE":
+          return;
+        default:
+          return;
+      }
     }
+    const { id, sender, content, timestamp, receiver } = parsedMessage;
+    setMessages((prevMessages) => {
+      const newMessages = [
+        ...prevMessages,
+        {
+          id: id,
+          sender: sender,
+          content: content,
+          timestamp: timestamp,
+          receiver: receiver,
+        },
+      ];
+      return newMessages.length > 80 ? newMessages.slice(-80) : newMessages;
+    });
+  }
 
-    useEffect(() => {
-        if (client && isConnected) {
-            getAllMessages('public').then(setMessages)
-                .catch((error) => setError({ message: error.message || "An error occurred" }));
-            scrollToBottom();
-            const username = Cookies.get('username');
-            const publicMessagesSubscription = client.subscribe('/topic/chat/public', onChatMessageReceived);
-            const privateMessagesSubscription = client.subscribe('/user/' + username + '/topic/chat/public', onChatMessageReceived);
-            return () => {
-                publicMessagesSubscription.unsubscribe();
-                privateMessagesSubscription.unsubscribe();
-            };
-        }
-    }, [client, isConnected]);
-
-    useEffect(() => {
-        if (isScrolledToBottom()) {
-            scrollToBottom();
-        }
-    }, [messages]);
-
-    function handleSendMessage() {
-        const token = Cookies.get('token');
-        const username = Cookies.get('username');
-        const messageContent = messageInputRef.current.value.trim();
-        if (!messageContent) {
-            messageInputRef.current.value = '';
-            return;
-        }
-        if (messageContent.length > 250) {
-            setNotifications(prev => [...prev, {
-                message: 'Message exceeds 300 characters',
-                duration: 3500,
-                isError: true
-            }]);
-            return;
-        }
-        if (!client || !client.publish) {
-            setNotifications(prev => [...prev, {
-                message: 'Client is not initialized or publish method is not available',
-                duration: 3500,
-                isError: true
-            }]);
-            return;
-        }
-        const [command, param] = messageContent.split(' ').filter(Boolean);
-        try {
-            switch (command) {
-                case '/clear':
-                    if (param) {
-                        const clearCount = parseInt(param, 10);
-                        client.publish({
-                            destination: '/app/chat/clear/public',
-                            headers: {
-                                Authorization: `Bearer ${token}`,
-                                username: username,
-                                clearCount: clearCount
-                            }
-                        });
-                    } else {
-                        client.publish({
-                            destination: '/app/chat/clear/public',
-                            headers: {
-                                Authorization: `Bearer ${token}`,
-                                username: username,
-                                clearCount: 'All'
-                            }
-                        });
-                    }
-                    break;
-                default:
-                    client.publish({
-                        destination: '/app/chat/sendPublicMessage/public',
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                            username: username
-                        },
-                        body: JSON.stringify({ content: messageContent })
-                    });
-            }
-
-            console.log('Sending message: ' + messageContent);
-            messageInputRef.current.value = '';
-            scrollToBottom();
-        } catch (error) {
-            setNotifications(prev => [...prev, {
-                message: 'Error sending message (no connection)',
-                duration: 3500,
-                isError: true
-            }]);
-        }
+  useEffect(() => {
+    if (client && isConnected) {
+      getAllMessages("public")
+        .then(setMessages)
+        .catch((error) =>
+          setError({ message: error.message || "An error occurred" })
+        );
+      scrollToBottom();
+      const username = Cookies.get("username");
+      const publicMessagesSubscription = client.subscribe(
+        "/topic/chat/public",
+        onChatMessageReceived
+      );
+      const privateMessagesSubscription = client.subscribe(
+        "/user/" + username + "/topic/chat/public",
+        onChatMessageReceived
+      );
+      return () => {
+        publicMessagesSubscription.unsubscribe();
+        privateMessagesSubscription.unsubscribe();
+      };
     }
+  }, [client, isConnected]);
 
-    function handleKeyDown(event) {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            handleSendMessage();
-        }
+  useEffect(() => {
+    if (isScrolledToBottom()) {
+      scrollToBottom();
     }
+  }, [messages]);
 
-    function handleInputChange(event) {
-        if (event.target.value.length > 250) {
-            event.target.value = event.target.value.slice(0, 250);
-        }
+  function handleSendMessage() {
+    const token = Cookies.get("token");
+    const username = Cookies.get("username");
+    const messageContent = messageInputRef.current.value.trim();
+    if (!messageContent) {
+      messageInputRef.current.value = "";
+      return;
     }
-
-    function isScrolledToBottom() {
-        const { scrollTop, scrollHeight, clientHeight } = chatContainerRef.current;
-        return scrollHeight - scrollTop < clientHeight + 80 && scrollHeight - scrollTop > clientHeight - 80;
+    if (messageContent.length > 250) {
+      setNotifications((prev) => [
+        ...prev,
+        {
+          message: "Message exceeds 300 characters",
+          duration: 3500,
+          isError: true,
+        },
+      ]);
+      return;
     }
-
-    function scrollToBottom() {
-        chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    if (!client || !client.publish) {
+      setNotifications((prev) => [
+        ...prev,
+        {
+          message:
+            "Client is not initialized or publish method is not available",
+          duration: 3500,
+          isError: true,
+        },
+      ]);
+      return;
     }
+    const [command, param] = messageContent.split(" ").filter(Boolean);
+    try {
+      switch (command) {
+        case "/clear":
+          if (param) {
+            const clearCount = parseInt(param, 10);
+            client.publish({
+              destination: "/app/chat/clear/public",
+              headers: {
+                Authorization: `Bearer ${token}`,
+                username: username,
+                clearCount: clearCount,
+              },
+            });
+          } else {
+            client.publish({
+              destination: "/app/chat/clear/public",
+              headers: {
+                Authorization: `Bearer ${token}`,
+                username: username,
+                clearCount: "All",
+              },
+            });
+          }
+          break;
+        default:
+          client.publish({
+            destination: "/app/chat/sendPublicMessage/public",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              username: username,
+            },
+            body: JSON.stringify({ content: messageContent }),
+          });
+      }
 
+      console.log("Sending message: " + messageContent);
+      messageInputRef.current.value = "";
+      scrollToBottom();
+    } catch (error) {
+      setNotifications((prev) => [
+        ...prev,
+        {
+          message: "Error sending message (no connection)",
+          duration: 3500,
+          isError: true,
+        },
+      ]);
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSendMessage();
+    }
+  }
+
+  function handleInputChange(event) {
+    if (event.target.value.length > 250) {
+      event.target.value = event.target.value.slice(0, 250);
+    }
+  }
+
+  function isScrolledToBottom() {
+    const { scrollTop, scrollHeight, clientHeight } = chatContainerRef.current;
     return (
-        <section className="chat">
-            <div className="chat__title title-box">Public Chat</div>
-            <div className="chat__text scroll" id="chat" ref={chatContainerRef}>
-                {!error && messages
-                    .map((message, index) => (
-                        <Message key={index} nickname={message.sender.nickname}>
-                            {message.content}
-                        </Message>
-                    ))}
-                {error && <p>{error.message}</p>}
-            </div>
-
-            <div className="chat__typing">
-                <input
-                    type="text"
-                    className="chat__typing-input"
-                    ref={messageInputRef}
-                    onKeyDown={handleKeyDown}
-                    onChange={handleInputChange}
-                    maxLength="300"
-                />
-                <button className="chat__typing-btn" onClick={handleSendMessage}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="chat__typing-btn-svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-                        />
-                    </svg>
-                </button>
-            </div>
-        </section>
+      scrollHeight - scrollTop < clientHeight + 80 &&
+      scrollHeight - scrollTop > clientHeight - 80
     );
+  }
+
+  function scrollToBottom() {
+    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+  }
+
+  return (
+    <section className="chat">
+      <div className="chat__title title-box">Public Chat</div>
+      <div className="chat__text scroll" id="chat" ref={chatContainerRef}>
+        {!error &&
+          messages.map((message, index) => (
+            <Message key={index} nickname={message.sender.nickname}>
+              {message.content}
+            </Message>
+          ))}
+        {error && <p>{error.message}</p>}
+      </div>
+
+      <div className="chat__typing">
+        <textarea
+          type="text"
+          className="chat__typing-input scroll"
+          ref={messageInputRef}
+          onKeyDown={handleKeyDown}
+          onChange={handleInputChange}
+          maxLength="300"
+        ></textarea>
+        <button className="chat__typing-btn" onClick={handleSendMessage}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="chat__typing-btn-svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+            />
+          </svg>
+        </button>
+      </div>
+    </section>
+  );
 }
--- frontend/src/components/chat/PrivateChatDialog.jsx
+++ frontend/src/components/chat/PrivateChatDialog.jsx
@@ -1,12 +1,144 @@
 import "./styles.css";
+
 import { createPortal } from "react-dom";
 
 export default function PrivateChatDialog() {
+  return createPortal(
+    <dialog open className="chat-dialog">
+      <div className="user-contacts">
+        <div className="search-user-contacts-div">
+          <input
+            type="text"
+            className="search-user-contacts search-user-contacts-input "
+            placeholder="Find User"
+          ></input>
 
-    return createPortal(
-        <dialog open className="chat-dialog">
-
-        </dialog>,
-        document.getElementById("modal")
-    );
+          <div className="tooltip">
+            <div className="tooltip-div scroll">
+              <div className="tooltip-div-p">
+                <p className="tooltip-div-text">аFFFFFFFFFFFFFFFFFFFFFFFFFFF</p>
+              </div>
+              <div className="tooltip-div-p">
+                <p className="tooltip-div-text">аFWEFFWFWWFWE</p>
+              </div>
+              <div className="tooltip-div-p">
+                <p className="tooltip-div-text">а</p>
+              </div>
+              <div className="tooltip-div-p">
+                <p className="tooltip-div-text">а</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="your-contacts scroll">
+          <div className="your-contact your-contact-active">
+            <h2 className="your-contact-neekname">Username</h2>
+            <p className="your-contact-p">fffffffffffffffffffffffffffffffff</p>
+          </div>
+          <div className="your-contact">
+            <h2 className="your-contact-neekname">Username</h2>
+            <p className="your-contact-p">fffffffffffffffffffffffffffffffff</p>
+          </div>
+          <div className="your-contact">
+            <h2 className="your-contact-neekname">Username</h2>
+            <p className="your-contact-p">fffffffffffffffffffffffffffffffff</p>
+          </div>
+          <div className="your-contact">
+            <h2 className="your-contact-neekname">Username</h2>
+            <p className="your-contact-p">fffffffffffffffffffffffffffffffff</p>
+          </div>
+          <div className="your-contact">
+            <h2 className="your-contact-neekname">Username</h2>
+            <p className="your-contact-p">fffffffffffffffffffffffffffffffff</p>
+          </div>
+          <div className="your-contact">
+            <h2 className="your-contact-neekname">Username</h2>
+            <p className="your-contact-p">fffffffffffffffffffffffffffffffff</p>
+          </div>
+        </div>
+      </div>
+      <div className="choosen-user">
+        <div className="user-and-close">
+          <a className="user-and-close-a">Username</a>
+          <button type="button" className="dialog-close">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="kick-svg2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="chat-zone scroll">
+          <div className="first appendix">
+            <div className="message your">
+              <div className="message-content">
+                Привіт! Це приклад повідомлення в стилі Telegram.
+              </div>
+            </div>
+          </div>
+          <div className="first  appendix">
+            <div className="message not-your">
+              <div className="message-content">
+                Привіт! Це приклад повідомлення в стилі Telegram.
+              </div>
+            </div>
+          </div>
+          <div className="first ">
+            <div className="message your">
+              <div className="message-content">
+                Привіт! Це приклад повідомлення в стилі
+                Telegram.fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <div className="message your">
+              <div className="message-content">
+                Привіт! Це приклад повідомлення в стилі Telegram.
+              </div>
+            </div>
+          </div>
+          <div className="appendix ">
+            <div className="message your">
+              <div className="message-content">
+                Привіт! Це приклад повідомлення в стилі Telegram.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="chat__typing chat__typing-dialog">
+          <textarea
+            className="chat__typing-input chat__typing-input-dialog scroll"
+            type="text"
+          ></textarea>
+          <button className="chat__typing-btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="chat__typing-btn-svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </dialog>,
+    document.getElementById("modal")
+  );
 }
